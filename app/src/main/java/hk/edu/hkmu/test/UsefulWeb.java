18e46fb36a561043468154e212d3461d1153a4f2
FinnTheHuman411 <lbenson997@gmail.com>
2022-04-25 23:13:07 +0800

    Tweaked "UsefulWeb"

--- app/src/main/java/hk/edu/hkmu/test/UsefulWeb.java
+++ app/src/main/java/hk/edu/hkmu/test/UsefulWeb.java
@@ -2,13 +2,28 @@ package hk.edu.hkmu.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UsefulWeb extends AppCompatActivity {
+
+    private Button backbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_useful_web);
+
+        backbutton = findViewById(R.id.back_button);
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(UsefulWeb.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
